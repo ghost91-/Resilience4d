878b2c538b3234c8de37724cd68b4707b27abde1
Johannes Loher <johannes.loher@fg4f.de>
2018-12-27 05:11:04 +0100

    feat: implement recover

--- fallback/source/resilience4d/fallback/package.d
+++ fallback/source/resilience4d/fallback/package.d
@@ -43,3 +43,82 @@ unittest
     failedResult.exception.msg.should.be == "Division by zero";
 
 }
+
+private template isImplicitlyConvertableInSomeDirection(T, U)
+{
+    enum isImplicitlyConvertableInSomeDirection = is(T : U) || is(U : T);
+}
+
+private template isCallableWith(alias func, U, V)
+{
+    enum isCallableWith = __traits(compiles, { U u = func(V.init); });
+}
+
+template recover(alias func)
+{
+    import std.traits : isSomeFunction, Parameters, ReturnType;
+
+    auto recover(T)(Expected!T self)
+            if ((isSomeFunction!func && is(ReturnType!func : T)
+                && isImplicitlyConvertableInSomeDirection!(Exception, Parameters!func))
+                || isCallableWith!(func, T, Exception))
+    {
+
+        if (!self.hasValue)
+        {
+            static if (isSomeFunction!func)
+            {
+                alias Param = Parameters!func[0];
+                static if (is(Exception : Param))
+                {
+                    return expected(func(self.exception));
+                }
+                else
+                {
+                    if (typeid(self.exception) == typeid(Param))
+                    {
+                        return expected(func(cast(Param) self.exception));
+                    }
+                }
+            }
+            else
+            {
+                return expected(func(self.exception));
+            }
+        }
+
+        return self;
+    }
+}
+
+///
+unittest
+{
+    // given
+    class MyException : Exception
+    {
+        this()
+        {
+            super("");
+        }
+    }
+
+    class AnotherException : Exception
+    {
+        this()
+        {
+            super("");
+        }
+    }
+
+    immutable val = unexpected!int(new AnotherException);
+
+    // when
+    immutable result = val.recover!((MyException x) => 0)
+        .recover!((AnotherException x) => 1)
+        .recover!((Exception x) => 2);
+
+    // then
+    result.hasValue.should.be == true;
+    result.value.should.be == 1;
+}
